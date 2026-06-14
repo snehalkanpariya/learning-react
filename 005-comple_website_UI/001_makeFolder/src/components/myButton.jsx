85053b9c3ff490cf7a6e19fea07f1b7e4501f1fc
Snehal Kanpariya <snehalkanpariya@gmail.com>
2026-06-14 17:52:34 +0530

    Delete 005-comple_website_UI/001_makeFolder directory

--- 005-comple_website_UI/001_makeFolder/src/components/myButton.jsx
+++
@@ -1,7 +0,0 @@
-function MyButton(){
-    return(
-        <button className='btn'> Visit Profile </button>
-    )
-}
-
-export default MyButton
--- 005-comple_website_UI/001_makeFolder/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client' // 👈 Crucial! This is the tool that renders your app
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-    <App />
-)
